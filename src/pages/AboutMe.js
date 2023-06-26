4212ae41fe376ca119c4d098884753c3f8196cd8
Willlasbury <asburyw229@gmail.com>
2023-06-26 10:06:57 -0700

    started to write an about me

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -3,6 +3,47 @@ export default function AboutMe({ visited }) {
     <>
       <h2>Thank you for taking the time to look at this page.</h2>
       <p> 
+        I started coding while I was a student at East Tennessee State University in 2015. During my last few years of college, I began doing research under on the binary star system Epislon Aurigea.
+        At the start of this process my professor recommended that I start to learn Python. With that direction I self taught and presented my data analyses at the Tennessee Learning Symposium.
+        Later on, as part of my computational physics course, I was taught Fortran77 and IDL. Since then I have done smaller projects and occassional coding challenges to keep up a base line of skill
+        over the years. 
+      </p>
+      <p>
+        After I finished college I worked as an ropes course manager leading team building groups, until I eventually found a job as a part time Lab Analyst with Eastman Chemical Company in Kingsport TN. 
+        I started in an apprentice position working in their gas chromatography area, and worked for about six months, until I gained full time employment. From there I continued in the apprentice position,
+        and started to learn liquid chromatography, various inductively coupled plasma (ICP) techniques, and a few others. 
+      </p>
+      <p>
+        While I was still in my apprenticeship phase of working, which was hard set at three years, I finished my continued education hours two years in ahead of schedule. Thanks to this, I was able to fill
+        an empty trainer postion on my crew. As part of my new role I trained analysts in our cleanroom and ICP areas of the lab. So for a year while working at Eastman I help the dual role of apprentice
+        and trainer, which I find fairly ironic.
+      </p>
+      <p>
+        At the same time, my wife, who was a nurse, and I had been building up a savings pool and had been planning to quit our jobs to travel the country in the van we had built. A few months before we
+        planned to quit, the pandemic began in the US. Also ironically, we postponed our road trip plans and kept working longer than we planned, largely because my wife felt a sense of resposibility to
+        keep working; however, Tennessee did not see any fallout for months. Due to this a variety of other factors, my wife and I felt it was okay to restart our road trip plan.
+      </p>
+      <p>
+        We left Tennessee in June of 2020, and travelled to Pennsylvania first and then slowly worked our way west, taking a fairly norther route. We were able to focus entirely on rock climbing during 
+        this time, and explore amazing places like the Red River Gorge, New River Gorge, Spearfish Canyon, Wild Iris, Indian Creek, and others. After six months of this amazing life style, my wife picked
+        up a job as a travel nurse in Salt Lake City. Originally I was planning to pick up a job working at a ski resort outside Salt Lake while my wife worked, however on our first day climbing in Little
+        Cotton Wood Canyon just outside Salt Lake, I had an accident while climbing (I fell and for some rease "decided" to kick a rock on my way down) and ended up unable to walk for a couple months.
+      </p>
+      <p>
+        While this was an unfortunate incident, it did lead to a few humerous situations, including me chasing a chicken with one foot and numerous days of climbing with crutches at the climbing gym. Mainly
+        it kept me from finding any work that requireed me to be on my feet; and being only a short term resident, I was unable to find employment in any timely manner. So, with nothing to do I decided it
+        probably time to pick up my programming habits again. 
+      </p>
+      <p>
+        I was quickly realize how rusty I was at anything past looping over an array, and reached out to good friend, who was much more experienced than me, to do some pair programming. With his help
+        I built a basic API built with Python and FastAPI. While this project did not end up going far, it was my first introduction to working with RESTful routing and building applications. After working
+        on this for about two months, and slowly realizing that building a web app with python was like hammering a nail with screwdriver, my wife finished her contract in Salt Lake City. So we left Salt 
+        Lake, and our consistent internet connection, and headed to California to climb in Bishop. 
+      </p>
+      <p>
+        I tried to keep up my motivation to code, but after about a month of no internet, I eventually dropped my project entirely. Instead I focused on all the amazing climbing around us. I spent a month
+        bouldering in the Volcanic Tablelands, outside Bishop, and then another month in the Buttermilk Boulders at the base of Mount Tom futher outside town. This time largely filled with making new friends,
+        climbing, and generally just having fun and living. Eventually though it started to get hot in California and we started to head north, with the goal of reaching Seattle for another nursing contract.
       </p>
     </>
   );
