d6575d56683975c74999fd6511e99d885c77247e
Willlasbury <asburyw229@gmail.com>
2023-07-25 09:57:55 -0600

    added links to github repos on projects

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,9 +1,11 @@
+import {Link} from 'react'
+
 export default function Repo ({repo}) {
     // TODO: pull metrics from repo data to display graphics
     return(
-        <section className="border-2 border-black rounded-lg px-3 py-1 m-2 shadow-md">
+        <a href={repo.html_url} target="_blank" className="border-2 border-black rounded-lg px-3 py-1 m-2 shadow-md hover:shadow-xl">
         <h2 className="text-slate-900 font-semibold">{repo.description}</h2>
         <h3 className="text-slate-800">{repo.html_url}</h3>
-        </section>
+        </a>
     )
 }
